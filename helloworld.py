939ad28053b9801e526bcf2730ab95b399adf431
UsaidAkmalLino <159388899+UsaidAkmalLino@users.noreply.github.com>
2024-06-27 10:48:06 +0700

    Create helloworld.py
    print helloworld text to the screen using the python programming language

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+#MENCETAK HELLO WORLD/STRING
+
+print("hello world and society") # Jika tipe datanya adalah string maka programmer wajib menambahk kan kutip("") pada nilai atau value 
+
+print("selamat belajar python")
